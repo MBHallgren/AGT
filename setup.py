631d623434631c2d07ff26fe7e6e873044a8c34c
MBHallgren <s153002@student.dtu.dk>
2023-04-05 10:36:43 +0200

    keep2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kmergenetyper',
-    version='1.0.2',
+    version='1.0.3',
     packages=find_packages(),
     data_files=[],
     include_package_data=True,

c7b140a6f1ff16f68ada440a21b97691ecf84d3c
MBHallgren <s153002@student.dtu.dk>
2023-04-21 09:49:53 +0200

    version fix

--- setup.py
+++ setup.py
@@ -4,11 +4,13 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
+from kmergenetyper.version import __version__
+
 setup(
     name='kmergenetyper',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='1.0.7',
+    version=__version__,
     packages=find_packages(),
     data_files=[],
     include_package_data=True,

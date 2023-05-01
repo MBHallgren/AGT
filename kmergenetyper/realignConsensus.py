31e7a94074f5e110922e43e6fa2a83ff73d2e0ab
MBHallgren <s153002@student.dtu.dk>
2023-05-01 15:35:42 +0200

    alignment msg7

--- kmergenetyper/realignConsensus.py
+++ kmergenetyper/realignConsensus.py
@@ -5,19 +5,12 @@ def realign_consensus(output, prefix, database, keep):
     non_perfect_hits = []
     alignment_dict = {}
     headers = ''
-    with open('{}/{}.res'.format(output, prefix), 'r') as f:
-        for line in f:
-            line = line.strip()
-            if not line.startswith('#'):
-                line = line.split('\t')
-                if float(line[4].strip()) != 100.00 or float(line[5].strip()) != 100.00 or float(line[6].strip()) != 100.00: #Non perfect alignment for Template_Identity	Template_Coverage	Query_Identity
-                    non_perfect_hits.append('>' + line[0].strip())
-                alignment_dict[line[0]] = []
-                for item in line[1:-1]:
-                    alignment_dict[line[0]].append(float(item.strip()))
-                alignment_dict[line[0]].append(line[-1].strip())
-            else:
-                headers = line
+
+    headers, alignment_dict = load_kma_res_file('{}/{}.res'.format(output, prefix))
+
+    for item in alignment_dict:
+        if alignment_dict[item][3] != 100.00 or alignment_dict[item][4] != 100.00 or alignment_dict[item][5] != 100.00:
+            non_perfect_hits.append('>' + item)
 
     alignment_dict = reformat_dict(alignment_dict)
 
@@ -52,11 +45,26 @@ def reformat_dict(input_dict):
         for value in input_dict[item]:
             output_dict[item.strip()].append(value.strip())
     return output_dict
+
+def load_kma_res_file(file)
+    kma_dict = dict()
+    with open(file, 'r') as f:
+        for line in f:
+            if line.startswith('#'):
+                header = line.rstrip()
+            else:
+                line = line.rstrip()
+                line = line.split('\t')
+                kma_dict[line[0]] = []
+                for item in line[1:-1]:
+                    kma_dict[line[0]].append(float(item.strip()))
+                kma_dict[line[0]].append(line[-1].strip())
+    return header, kma_dict
 def eval_realignments(output, prefix, headers, alignment_dict, non_perfect_hits):
     realignment_dict = {}
 
     for item in alignment_dict:
-        if float(alignment_dict[item][3]) == 100.00 and float(alignment_dict[item][4]) == 100.00 and float(alignment_dict[item][5]) == 100.00: #Perfect alignment for Template_Identity	Template_Coverage	Query_Identity
+        if alignment_dict[item][3] == 100.00 and alignment_dict[item][4] == 100.00 and alignment_dict[item][5] == 100.00: #Perfect alignment for Template_Identity	Template_Coverage	Query_Identity
             realignment_dict[item] = alignment_dict[item]
 
     print (alignment_dict)
@@ -64,29 +72,25 @@ def eval_realignments(output, prefix, headers, alignment_dict, non_perfect_hits)
     print (realignment_dict)
 
     for item in non_perfect_hits:
-        with open('{}/{}.res'.format(output, item[1:]), 'r') as f:
-            original_gene = item[1:]
-            for line in f:
-                line = line.rstrip()
-                if not line.startswith('#'):
-                    gene = line.split('\t')[0].strip()
-                    print ('Gene: {}'.format(gene))
-                    if gene not in realignment_dict:
-                        print ('Gene not in realignment_dict:', gene)
-                        realignment_dict[gene] = alignment_dict[original_gene]
-                        realignment_dict[gene][3] = float(line.split('\t')[4]) # Replace template identity
-                        realignment_dict[gene][4] = float(line.split('\t')[5])  # Replace template coverage
-                        realignment_dict[gene][5] = float(line.split('\t')[6])  # Replace query identity
-                        realignment_dict[gene][6] = float(line.split('\t')[7])  # Replace Query_Coverage
-                        realignment_dict[gene][7] = alignment_dict[original_gene][7]  # Replace depth
-                    else:
-                        realignment_dict[gene][3] = float(line.split('\t')[4]) #Replace template identity
-                        realignment_dict[gene][4] = float(line.split('\t')[5]) #Replace template coverage
-                        realignment_dict[gene][5] = float(line.split('\t')[6])  # Replace query identity
-                        realignment_dict[gene][6] = float(line.split('\t')[7])  # Replace Query_Coverage
-                        realignment_dict[gene][7] = max(float(alignment_dict[original_gene][7]), float(alignment_dict[gene][7])) # Select max depth
-                        realignment_dict[gene][8] = max(float(alignment_dict[original_gene][8]), float(alignment_dict[gene][8])) # Select max q_value
-                        realignment_dict[gene][9] = max(float(alignment_dict[original_gene][9]), float(alignment_dict[gene][9])) # Select max p_value
+        headers, currect_gene_dict = load_kma_res_file('{}/{}.res'.format(output, item[1:]))
+        original_gene = item[1:]
+
+        for gene in currect_gene_dict:
+            if gene not in realignment_dict:
+                print ('Gene not in realignment_dict:', gene)
+                realignment_dict[gene] = alignment_dict[original_gene]
+                realignment_dict[gene][3] = float(currect_gene_dict[gene][3]) #Replace template identity
+                realignment_dict[gene][4] = float(currect_gene_dict[gene][4]) #Replace template coverage
+                realignment_dict[gene][5] = float(currect_gene_dict[gene][5])  # Replace query identity
+                realignment_dict[gene][6] = float(currect_gene_dict[gene][6])  # Replace Query_Coverage
+            else:
+                print ('Gene in realignment_dict:', gene)
+                realignment_dict[gene][3] = float(currect_gene_dict[gene][3]) #Replace template identity
+                realignment_dict[gene][4] = float(currect_gene_dict[gene][4]) #Replace template coverage
+                realignment_dict[gene][5] = float(currect_gene_dict[gene][5])  # Replace query identity
+                realignment_dict[gene][6] = float(currect_gene_dict[gene][6])  # Replace Query_Coverage
+                realignment_dict[gene][7] = max(float(alignment_dict[original_gene][7]), float(currect_gene_dict[gene][7])) # Select max depth
+                realignment_dict[gene][8] = max(float(alignment_dict[original_gene][8]), float(currect_gene_dict[gene][8])) # Select max q_value
     realignment_dict = reformat_dict(realignment_dict)
 
     keys = list(realignment_dict.keys())

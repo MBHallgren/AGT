643743b28fbbcfd6da2dc9275bac0f1d16c98d8f
MBHallgren <s153002@student.dtu.dk>
2023-07-25 10:40:03 +0200

    nanopore23

--- kmergenetyper/typeGenes.py
+++ kmergenetyper/typeGenes.py
@@ -24,6 +24,13 @@ def type_genes(args):
                 prefix = prefix + '_nanopore'
             os.system('kma -i {} -o {}/{} -t_db {} -ont -md {} -ID 95 -eq {} -t {}'.format(item, args.output, prefix, args.t_db, args.md, args.q_score, args.threads))
             realignConsensus.realign_consensus(args.output, prefix, args.t_db, args.keep, args.threads)
+    if args.fasta != []:
+        for item in args.fasta:
+            prefix = derive_prefix(item)
+            if prefix in collections:
+                prefix = prefix + '_fasta'
+            os.system('kma -i {} -o {}/{} -t_db {} -md 0.1 -ID 95 -t {}'.format(item, args.output, prefix, args.t_db, args.threads))
+            realignConsensus.realign_consensus(args.output, prefix, args.t_db, args.keep, args.threads)
 
 def check_prefix_collisions(args):
     prefixes = []

130f1719b639d26bb83515e76bf1f22a8118b1fa
MBHallgren <s153002@student.dtu.dk>
2023-08-21 19:28:12 +0200

    1.1.17

--- make_conda.py
+++ make_conda.py
@@ -36,6 +36,7 @@ data = {
 }
 
 # Convert the data to YAML and print it
-yaml_str = yaml.dump(data, default_flow_style=False)
+yaml_str = yaml.dump(data, sort_keys=False)
+
 with open('conda/meta.yaml', 'w') as f:
     f.write(yaml_str)
